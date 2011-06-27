3b087aa0dd9ab08aa55681bf269e5f7ffa337a7f
Daniel Kehoe <kehoe@fortuity.com>
2011-06-26 17:01:37 -0700

    add Skeleton files for CSS & JS

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  before_filter :authenticate_user!
-  before_filter :correct_user?
+  # before_filter :authenticate_user!
+  # before_filter :correct_user?
 
     def edit
     @user = User.find(params[:id])
