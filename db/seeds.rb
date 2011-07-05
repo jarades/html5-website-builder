60a5b15e503dfd4b72201510356a7efcb9405b63
Daniel Kehoe <kehoe@fortuity.com>
2011-07-05 12:55:18 -0700

    create seed data for the app

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,27 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 puts 'EMPTY THE MONGODB DATABASE'
 Mongoid.master.collections.reject { |c| c.name =~ /^system/}.each(&:drop)
-Setting.create! :articles_directory => 'howto'
+Setting.create! :articles_directory => 'howto', :site_name => 'Rails Apps', :tagline => 'Rails 3.1 Example Apps and Tutorials'
 puts 'Set defaults for the app'
+
+data = 
+<<-RUBY
+h3. Installing the Application
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+
+h3. Getting Started
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+
+bq. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+RUBY
+
+Article.create! :title => 'Installing Rails 3.1',
+  :filename => 'installing-rails-3-1',
+  :description => 'Detailed instructions on how to install Rails 3.1',
+  :headline => 'Read This Before Installing Rails 3.1',
+  :subhead => 'What You Need to Know',
+  :content => data,
+  :published => false
+puts 'Created a sample article'
